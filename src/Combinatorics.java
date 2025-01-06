6cc57a92b9314185980bf49bf8e6805b387436b9
wenbo <wenbozhang12@gmail.com>
2025-01-05 22:55:13 -0800

    .

--- src/Combinatorics.java
+++ src/Combinatorics.java
@@ -2,11 +2,19 @@ public class Combinatorics {
     int mod = (int) 1e9 + 7;
     Long[] fac;
     long fac(int i){
+        if(i == 0)
+            return 1;
         if(fac[i] != null)
             return fac[i];
         return fac[i] = i * fac(i - 1) % mod;
     }
 
+    Long[] powDp;
+    long powDp(long b, long e){
+        if(powDp[0] != null)
+            return powDp[(int)b];
+        return powDp[(int)b] = pow(b, mod - 2);
+    }
     long pow(long b, long e){
         if(e == 0)
             return 1;
