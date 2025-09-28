9f27c526c847e66d48029c16d8c81b9a7b863d85
wenbo <wenbozhang12@gmail.com>
2025-09-27 23:17:25 -0400

    .

---
+++ src/Matrix.java
@@ -0,0 +1,27 @@
+public class Matrix {
+    long[][] mul(long[][] A, long[][] B) {
+        int aRows = A.length;
+        int aColumns = A[0].length;
+        int bRows = B.length;
+        int bColumns = B[0].length;
+
+        long[][] C = new long[aRows][bColumns];
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bColumns; j++) {
+                for (int k = 0; k < aColumns; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+
+    long[][] exp(long[][] A, long e){
+        if(e == 1)
+            return A;
+        if(e % 2 == 0){
+            return exp(mul(A, A), e/2);
+        }
+        return mul(exp(mul(A, A), e/2),A);
+    }
+}
